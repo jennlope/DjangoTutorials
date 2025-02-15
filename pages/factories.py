1e580dc396badbe56ce75e1f4d76bf306886b40c
Jennifer <lopezgomezjennifer50@hotmail.com>
2025-02-15 15:09:22 -0500

    tutorial 3

--- pages/factories.py
+++ pages/factories.py
@@ -1,5 +1,7 @@
 import factory 
 from .models import Product 
+
+
 class ProductFactory(factory.django.DjangoModelFactory): 
     class Meta: 
         model = Product 
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView,ProductIndexView,ProductShowView, ProductCreateView, SuccessView
+from .views import HomePageView, AboutPageView, ContactPageView,ProductIndexView,ProductShowView, ProductCreateView, SuccessView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('products/success/', SuccessView.as_view(), name='product_success'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
 ]
